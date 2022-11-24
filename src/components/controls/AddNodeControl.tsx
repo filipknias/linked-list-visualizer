2018788c439104a77d49c3153176b327a7e6b852
Filip <filip.knias@o2.pl>
2022-11-24 12:36:32 +0100

    change add node input type to number

--- src/components/controls/AddNodeControl.tsx
+++ src/components/controls/AddNodeControl.tsx
@@ -4,22 +4,17 @@ import Button from '../common/Button/Button';
 import { useLinkedList } from '../../context/LinkedListContext';
 
 export default function AddNodeControl() {
-  const [data, setData] = useState('');
+  const [data, setData] = useState(0)
   const { addNode } = useLinkedList();
-
-  const handleAddNode = () => {
-    if (data === "") return;
-    addNode(data);
-  }
 
   return (
     <Stack>
-      <Button onClick={handleAddNode}>Add</Button>
+      <Button onClick={() => addNode(data)}>Add</Button>
       <input 
-        type="text" 
+        type="number" 
         placeholder="Data"
         value={data}
-        onChange={(e) => setData(e.target.value)}
+        onChange={(e) => setData(parseInt(e.target.value))}
       />
     </Stack>
   )
