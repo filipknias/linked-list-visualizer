63d06969687ec3726f0df95e78e79d74a35f21a5
Filip <filip.knias@o2.pl>
2022-11-24 09:42:26 +0100

    set node control form created

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import Appbar from './components/app/Appbar/Appbar';
+import SetNodeControl from './components/controls/SetNodeControl';
 
 export default function App() {
   return (
     <div className="container">
       <Appbar />
+      <div style={{ width: '20%' }}>
+        <SetNodeControl />
+      </div>
     </div>
   )
 }
